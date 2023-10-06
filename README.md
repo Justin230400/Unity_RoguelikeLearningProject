f64c393b36658f2c8d83a8c76941f3759e2a7a45
Justin230400 <justinloh23@gmail.com>
2023-10-07 00:18:02 +0800

    Part 0 - Setting Up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Unity_RoguelikeLearningProject
 Learn to use Unity to make roguelike games
+#Unity Version: 2020.3.30f1
---
+++ Rogue Like Game Learning Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+}
